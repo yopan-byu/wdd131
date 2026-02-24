f24b6741d980479a5bd0c298433daf93ed269834
Leonidas Yopan <leonidasyopan@gmail.com>
2026-02-24 19:24:40 -0300

    Add base CSS styles and JavaScript for dynamic date display

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+const year = document.querySelector("#currentyear");
+const currentYear = new Date().getFullYear();
+year.innerHTML = `<span class="highlight">${currentYear}</span>`;
+
+const lastModified = document.getElementById("lastModified");
+lastModified.innerHTML = document.lastModified;
